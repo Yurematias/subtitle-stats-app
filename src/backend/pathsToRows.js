5a8e02f44265cd93ad9a854936a097d7438bfe1b
Yurematias <yurematias@ufrn.edu.br>
2020-05-05 21:30:48 -0300

    variable names changed

--- src/backend/pathsToRows.js
+++ src/backend/pathsToRows.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
 module.exports = paths => {
-    return new Promise((resolver, reject) => {
+    return new Promise((resolve, reject) => {
         try {
             const rows = paths
                 .map(path => fs.readFileSync(path).toString('utf-8'))
                 .reduce((fullText, fileText) => `${fullText}\n${fileText}`)
                 .split('\n');
-            resolver(rows);
+            resolve(rows);
         } catch (error) {
             reject(error);
         }
